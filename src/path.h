a8821db06a526f30c6a28ada676aad9ccaded62d
Martin Kunev <martinkunev@gmail.com>
2015-03-17 14:27:44 +0200

    GPLv3 licence information added

--- src/path.h
+++ src/path.h
@@ -1,3 +1,22 @@
+/*
+ * Filement Index
+ * Copyright (C) 2015  Martin Kunev <martinkunev@gmail.com>
+ *
+ * This file is part of Filement Index.
+ *
+ * Filement Index is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation version 3 of the License.
+ *
+ * Filement Index is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Filement Index.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 enum {PATH_SIZE_LIMIT = 4096};
 
 int db_path_init(char path[static PATH_SIZE_LIMIT]);
