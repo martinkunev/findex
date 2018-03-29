2bb70ed418b7f0c054268d7cafd4b1f0a21080d1
Martin Kunev <martinkunev@gmail.com>
2018-03-29 18:21:24 +0200

    add support for mtime display and multiple files in finfo

--- src/finfo.c
+++ src/finfo.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
+#include <time.h>
 #include <unistd.h>
 
 #include "base.h"
@@ -16,13 +17,17 @@
 static int usage(int code)
 {
 	write(1, STRING(
-"Usage: finfo <file>\n"
+"Usage: finfo <file> ...\n"
 	));
 	return code;
 }
 
 static void print(const char *name, size_t name_length, const struct file *restrict file)
 {
+	char modified[64]; // TODO make sure this is big enough
+	time_t mtime = file->mtime;
+	ctime_r(&mtime, modified);
+
 	struct string content = {0};
 
 	if (file->content & CONTENT_DIRECTORY)
@@ -63,32 +68,37 @@ static void print(const char *name, size_t name_length, const struct file *restr
 	if (content.count)
 		content.count -= 2;
 
-	const char *mtime = "";
-
-	printf("File: %s\nSize: %u\nModify: %s\nContent: %.*s\n", name, file->size, mtime, (int)content.count, content.data);
+	printf("File: %s\nSize: %u\nModify: %sContent: %.*s\n", name, file->size, modified, (int)content.count, content.data);
 }
 
 int main(int argc, char *argv[])
 {
-	struct stat info;
-	size_t length;
-	struct file file;
-	int status;
+	int i;
 
 	// TODO better error handling
 
-	if ((argc != 2) || !strcmp(argv[1], "--help"))
+	if ((argc < 2) || !strcmp(argv[1], "--help"))
 		return usage(0);
 
-	if (stat(argv[1], &info) < 0)
-		return ERROR;
+	for(i = 1; i < argc; i += 1)
+	{
+		int status;
+		struct stat info;
+		size_t length;
+		struct file file;
 
-	length = strlen(argv[1]);
-	status = db_file_info(&file, argv[1], length, &info);
-	if (status < 0)
-		return -status;
+		if (stat(argv[i], &info) < 0)
+			return ERROR;
 
-	print(argv[1], length, &file);
+		length = strlen(argv[i]);
+		status = db_file_info(&file, argv[i], length, &info);
+		if (status < 0)
+			return -status;
+
+		if (i > 1)
+			putc('\n', stdout); // separate output by new lines
+		print(argv[i], length, &file);
+	}
 
 	return 0;
 }
