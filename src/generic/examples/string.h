c17c824068a76cbbdbbdedcc782cb28d2ecc53fe
Martin Kunev <martinkunev@gmail.com>
2018-05-14 01:10:48 +0200

    Add missing generic library example

---
+++ src/generic/examples/string.h
@@ -0,0 +1,12 @@
+#define ARRAY_GLOBAL
+#define ARRAY_NAME string
+#define ARRAY_TYPE char
+#include "array.g"
+
+#define NAME_CAT_EXPAND(a, b) a ## b
+#define NAME_CAT(a, b) NAME_CAT_EXPAND(a, b)
+#define NAME(suffix) NAME_CAT(ARRAY_NAME, suffix)
+int NAME(_append)(struct ARRAY_NAME *restrict string, const ARRAY_TYPE *data, size_t count);
+#undef NAME
+#undef NAME_CAT
+#undef NAME_CAT_EXPAND
