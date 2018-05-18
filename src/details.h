914ed45acc05ee52b17632b1eb2cf882a2693d5c
Martin Kunev <martinkunev@gmail.com>
2018-05-19 01:00:47 +0200

    Use info format for information printing in ffind

---
+++ src/details.h
@@ -0,0 +1,3 @@
+struct file;
+
+void details(const char *name, size_t name_length, const struct file *restrict file);
--- src/finfo.c
+++ src/finfo.c
@@ -10,6 +10,7 @@
 #include "path.h"
 #include "db.h"
 #include "magic.h"
+#include "details.h"
 #include "array_string.h"
 
 #define STRING(s) (s), sizeof(s) - 1
@@ -20,55 +21,6 @@ static int usage(int code)
 "Usage: finfo <file> ...\n"
 	));
 	return code;
-}
-
-static void print(const char *name, size_t name_length, const struct file *restrict file)
-{
-	char modified[64]; // TODO make sure this is big enough
-	time_t mtime = file->mtime;
-	ctime_r(&mtime, modified);
-
-	struct string content = {0};
-
-	if (file->content & CONTENT_DIRECTORY)
-		if (string_append(&content, STRING("Directory, ")) < 0)
-			abort();
-    if (file->content & CONTENT_LINK)
-		if (string_append(&content, STRING("Symbolic link, ")) < 0)
-			abort();
-    if (file->content & CONTENT_SPECIAL)
-		if (string_append(&content, STRING("Special file, ")) < 0)
-			abort();
-    if (file->content & CONTENT_EXECUTABLE)
-		if (string_append(&content, STRING("Executable, ")) < 0)
-			abort();
-    if (file->content & CONTENT_TEXT)
-		if (string_append(&content, STRING("Text, ")) < 0)
-			abort();
-    if (file->content & CONTENT_ARCHIVE)
-		if (string_append(&content, STRING("Archive, ")) < 0)
-			abort();
-    if (file->content & CONTENT_DOCUMENT)
-		if (string_append(&content, STRING("Document, ")) < 0)
-			abort();
-    if (file->content & CONTENT_IMAGE)
-		if (string_append(&content, STRING("Image, ")) < 0)
-			abort();
-    if (file->content & CONTENT_AUDIO)
-		if (string_append(&content, STRING("Audio, ")) < 0)
-			abort();
-    if (file->content & CONTENT_VIDEO)
-		if (string_append(&content, STRING("Video, ")) < 0)
-			abort();
-    if (file->content & CONTENT_DATABASE)
-		if (string_append(&content, STRING("Database, ")) < 0)
-			abort();
-
-	// Ignore last comma and space.
-	if (content.count)
-		content.count -= 2;
-
-	printf("File: %s\nSize: %u\nModify: %sContent: %.*s\n", name, file->size, modified, (int)content.count, content.data);
 }
 
 int main(int argc, char *argv[])
@@ -115,7 +67,7 @@ int main(int argc, char *argv[])
 
 		if (i > 1)
 			putc('\n', stdout); // separate output by new lines
-		print(argv[i], length, &file);
+		details(argv[i], length, &file);
 	}
 
 	db_close(&search);
