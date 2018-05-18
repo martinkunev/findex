910222c06e88fdaed36d7661a34f46b8ad9cda32
Martin Kunev <martinkunev@gmail.com>
2018-05-19 00:22:05 +0200

    Fix indexing bugs and error handling

--- src/findex.c
+++ src/findex.c
@@ -19,8 +19,6 @@
 
 #include <dirent.h>
 #include <errno.h>
-#include <fcntl.h>
-#include <stddef.h>
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
@@ -37,13 +35,13 @@
 
 #define STRING(s) (s), sizeof(s) - 1
 
-static int db_insert(struct db *restrict db, char *path, size_t path_length, const struct stat *restrict info)
+static int db_insert(struct db *restrict db, const char *restrict path, size_t path_length, const struct stat *restrict info)
 {
 	struct file file;
 
 	int status = db_set_fileinfo(&file, path, path_length, info);
 	if (status < 0)
-		return status;
+		return ((status == ERROR_CANCEL) ? 0 : status); // ERROR_CANCEL is non-fatal
 
 	return db_add(db, path, path_length, &file);
 }
@@ -53,23 +51,8 @@ static int db_index(struct db *restrict db, char *path, size_t path_length)
 {
 	DIR *dir;
 	struct dirent *entry;
-#if !defined(READDIR)
-	struct dirent *more;
-#endif
-	struct stat info;
-
-	const char *name;
-	size_t name_length;
 
 	int status;
-
-/* TODO I should be able to delete this
-	if (path_length > PATH_SIZE_LIMIT)
-	{
-		status = ERROR_MEMORY;
-		goto finally;
-	}*/
-	path[path_length] = 0;
 
 	// TODO better error handling
 
@@ -79,29 +62,51 @@ static int db_index(struct db *restrict db, char *path, size_t path_length)
 		switch (errno)
 		{
 		case EACCES:
+			fprintf(stderr, "Permission denied to read %s\n", path);
 			return 0;
 
 		default:
+			fprintf(stderr, "Unable to open %s\n", path);
 			return ERROR;
 		}
 	}
 
-	entry = alloc(offsetof(struct dirent, d_name) + pathconf(path, _PC_NAME_MAX) + 1);
+#if !defined(READDIR)
+	entry = malloc(offsetof(struct dirent, d_name) + pathconf(path, _PC_NAME_MAX) + 1);
+	if (!entry)
+	{
+		closedir(dir);
+		return ERROR_MEMORY;
+	}
+#endif
+
+	path[path_length++] = '/';
 
 	while (1)
 	{
+		const char *name;
+		size_t name_length;
+		struct stat info;
+
 #if defined(READDIR)
 		errno = 0;
 		if (!(entry = readdir(dir)))
 		{
 			if (errno)
-				return ERROR;
-			else
-				break; // no more entries
+			{
+				status = ERROR;
+				goto finally;
+			}
+			else break; // no more entries
 		}
 #else
+		struct dirent *more;
+
 		if (readdir_r(dir, entry, &more))
-			return ERROR;
+		{
+			status = ERROR;
+			goto finally;
+		}
 		if (!more)
 			break; // no more entries
 #endif
@@ -117,45 +122,45 @@ static int db_index(struct db *restrict db, char *path, size_t path_length)
 		name_length = strlen(name);
 #endif
 
-		if (path_length + name_length > PATH_SIZE_LIMIT)
+		if (path_length + name_length + 1 > PATH_SIZE_LIMIT)
 		{
-			status = ERROR_MEMORY;
+			status = ERROR_UNSUPPORTED;
 			goto finally;
 		}
 		memcpy(path + path_length, name, name_length);
 		path_length += name_length;
 		path[path_length] = 0;
 
-		if (lstat(path, &info) < 0) return ERROR;
-
-		status = db_insert(db, path, path_length, &info);
-		if (status)
-			return status;
-
-		// Recursively index each directory.
-		if (S_ISDIR(info.st_mode))
+		if (lstat(path, &info) < 0)
 		{
-			if (path_length + 1 > PATH_SIZE_LIMIT)
-			{
-				status = ERROR_MEMORY;
-				goto finally;
-			}
-			path[path_length++] = '/';
+			fprintf(stderr, "Unable to lstat %s\n", path);
+			status = ERROR;
+			goto finally;
+		}
+
+		if (status = db_insert(db, path, path_length, &info))
+		{
+			fprintf(stderr, "Unable to insert entry %s\n", path);
+			goto finally;
+		}
+
+		// Recursively index each subdirectory.
+		if (S_ISDIR(info.st_mode))
 			if (status = db_index(db, path, path_length))
 				goto finally;
-			path_length -= 1;
-		}
 
 		path_length -= name_length;
 	}
 
+	path[path_length -= 1] = 0;
+
 	status = 0;
 
 finally:
-	closedir(dir);
 #if !defined(READDIR)
 	free(entry);
 #endif
+	closedir(dir);
 
 	return status;
 }
